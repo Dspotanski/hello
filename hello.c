b242bd4998c2b6097614a2443f1c1d955d21d53a
Dylan Spotanski <dylanspotanski@gmail.com>
2019-01-15 17:13:24 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Dylan Spotanski
+ * Date: 2019/01/14
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Dylan Spotanski!\n");
+
+  return 0;
+}
